ebb2c5de4c942c55a514fb04382ff4136392558b
Andre Luiz Da Silva Chiapetta <andreluizchiapetta@gmail.com>
2020-05-23 22:04:00 -0300

    aula16 estrutura de repetiçao

---
+++ aula16/ambiente.js
@@ -0,0 +1,23 @@
+var c = 0
+var i = 0
+while (c < 8) {
+    console.log(`Passo while ${c}`)
+    c++
+}
+
+do {
+    console.log(`Passo do ${i}`)
+    i++
+} while (i < 8)
+
+
+
+/*console.log(`Tudo bem??`)
+console.log(`Tudo bem??`)
+console.log(`Tudo bem??`)
+console.log(`Tudo bem??`)
+console.log(`Tudo bem??`)
+console.log(`Tudo bem??`)
+console.log(`Tudo bem??`)
+console.log(`Tudo bem??`)
+*/
